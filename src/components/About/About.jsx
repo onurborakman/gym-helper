abdd7b5559dead9efe24c0912ae6d490dd99ff1e
Onur Bora Akman <onurboraakman@Onurs-MacBook-Air.local>
2024-01-13 06:33:34 +0300

    eslint fixes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -11,10 +11,10 @@ const About = () => (
             <p>Our mission is clear: to empower individuals on their path to a healthier lifestyle through personalized fitness guidance and community support.</p>
 
             <h3>Personalized Fitness Solutions</h3>
-            <p>Gym Helpert provides tailor-made workout plans designed to meet your unique needs. Whether you're a beginner taking the first steps toward fitness or a seasoned pro targeting specific goals, our platform adapts to your level, ensuring a customized workout experience.</p>
+            <p>Gym Helpert provides tailor-made workout plans designed to meet your unique needs. Whether you&apos;re a beginner taking the first steps toward fitness or a seasoned pro targeting specific goals, our platform adapts to your level, ensuring a customized workout experience.</p>
 
             <h3>Progress Tracking Made Easy</h3>
-            <p>Stay motivated with Gym Helpert's intuitive progress tracking tools. Celebrate milestones, monitor your achievements, and maintain focus on your fitness journey. Clear visualization of your progress keeps you inspired to reach new heights.</p>
+            <p>Stay motivated with Gym Helpert&apos;s intuitive progress tracking tools. Celebrate milestones, monitor your achievements, and maintain focus on your fitness journey. Clear visualization of your progress keeps you inspired to reach new heights.</p>
 
             <h3>Community Connection</h3>
             <p>Join our supportive community of fitness enthusiasts. Share accomplishments, seek advice, and find inspiration within a like-minded community. Together, we can reach new heights in health and wellness.</p>
@@ -23,7 +23,7 @@ const About = () => (
             Your well-being is our top priority. Gym Helpert is committed to providing a user-friendly platform backed by cutting-edge fitness technology. We continuously improve features and expand resources to ensure a safe and positive experience for our users.
 
             <h3>Start Your Fitness Journey Today</h3>
-            <p>Embark on a transformative fitness journey with Gym Helpert. Whether you're a beginner or an expert, we are here to guide, motivate, and celebrate every step of your progress. Let's achieve your fitness goals together!</p>
+            <p>Embark on a transformative fitness journey with Gym Helpert. Whether you&apos;re a beginner or an expert, we are here to guide, motivate, and celebrate every step of your progress. Let&apos;s achieve your fitness goals together!</p>
     </div>
 )
 
